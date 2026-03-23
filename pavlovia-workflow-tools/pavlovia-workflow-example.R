1cf838ffba530b282653196c768f4fc85a11e922
slicesofdata <54966189+slicesofdata@users.noreply.github.com>
2026-03-22 21:57:46 -0700

    added instructional elements and data flow

--- pavlovia-workflow-tools/pavlovia-workflow-example.R
+++ pavlovia-workflow-tools/pavlovia-workflow-example.R
@@ -13,13 +13,27 @@
 ################################################################################
 # define functions
 source(here::here("pavlovia-workflow-tools", "pavlovia-workflow-functions.R"))
-################################################################################
-# usage for token 
-pavlovia_create_and_store_personal_access_token()
-# setup directory (one time)
-pavlovia_setup_repo_directory()
-# clone repo (one time)
-pavlovia_clone_repo()
+
+# configure configure and clone (step by step)
+repo_cloned = FALSE # set to TRUE once cloned initially
+# get and set token
+if (!repo_cloned) {
+  # create token as necessary
+  pavlovia_create_and_store_personal_access_token()
+}
+# build directory structure
+if (!repo_cloned) {
+  # setup directory (one time)
+  pavlovia_setup_repo_directory()
+}
+# clone repository
+if (!repo_cloned) {
+  # clone repo (one time)
+  pavlovia_clone_repo(user_name = "your-pavlovia-username", 
+                      repo_name = "experiment-repo-name"
+                      )
+}
+# end setup
 
 ################################################################################
 # data processing workflow: pull and copy
@@ -28,4 +42,15 @@ pavlovia_pull_repo()
 pavlovia_copy_and_push_data()
 
 ################################################################################
-# 
+################## Begin data processing #######################################
+################################################################################
+# load libraries
+
+################################################################################
+# read data 
+
+################################################################################
+# clean data
+
+################################################################################
+# end
